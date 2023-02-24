8a995e344fff30560f2077bf1c7a03ce6163eecb
Soner Sayakci <s.sayakci@shopware.com>
2023-02-24 16:04:40 +0100

    feat: auto configure merge and update

--- main.tf
+++ main.tf
@@ -31,12 +31,16 @@ resource "github_repository" "repositories" {
   name                   = each.key
   description            = each.value.description
   has_issues             = try(each.value.has_issues, true)
+  topics                 = try(each.value.topics, [])
   has_projects           = false
   has_wiki               = false
   auto_init              = true
   delete_branch_on_merge = true
   homepage_url           = try(each.value.homepage_url, "")
   vulnerability_alerts   = true
+  allow_auto_merge       = true
+  allow_update_branch    = true
+  archive_on_destroy     = true
 
   lifecycle {
     prevent_destroy = true
